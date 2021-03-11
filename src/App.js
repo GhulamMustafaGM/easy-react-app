fd6cfa46e5ce05013eb90793b7acaf570b636e2f
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-11 18:56:28 +0100

    creat reactRouter DOM

--- src/App.js
+++ src/App.js
@@ -3,15 +3,40 @@ import About from './components/about';
 import Contact from './components/contact';
 import Portfolio from './components/portfolio';
 import Home from './components/home';
+import { 
+    BrowserRouter as Router,
+    Route, Switch, Link
+} from 'react-router-dom';
 
 function App() {
     return ( 
-        <div className="App">
-            <h1>Hi World!</h1>
-            <About />
-            <Contact />
-            <Portfolio />
-        </div>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                        <li>
+                            <Link to="/Portfolio">Portfolio</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/contact" component={Contact} />
+                    <Route exact path="/portfolio" component={Portfolio} />
+                </Switch>
+            </div>
+        </Router>
     );
 }
+
 export default App;
