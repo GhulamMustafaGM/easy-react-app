06d8cfa8d435d13b5898d8bf945e40db87a08037
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-11 20:16:12 +0100

    created display images

--- src/components/about.js
+++ src/components/about.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Lion from '../images/lion.jpg';
 
 export default class about extends Component {
     render() {
         return (
-            <div>
+            <div className="container">
                 <h1>About Page</h1>
+                <img src={Lion} alt="" style={{ width:500 }} />
             </div>
         )
     }
