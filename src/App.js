3e553ebdde89be105acbdcec9690b84008b58f2a
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-11 18:32:27 +0100

    created contact component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import About from './components/about';
+import Contact from './components/contact';
 
 function App() {
     return ( 
         <div className="App">
             <h1>Hi World!</h1>
             <About/>
+            <Contact/>
         </div>
     );
 }
