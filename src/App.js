2ce11277440c5f998c1737f3eb1bc653ad63348c
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-11 18:35:40 +0100

    created portfolio component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import About from './components/about';
 import Contact from './components/contact';
+import Portfolio from './components/portfolio';
 
 function App() {
     return ( 
         <div className="App">
             <h1>Hi World!</h1>
-            <About/>
-            <Contact/>
+            <About />
+            <Contact />
+            <Portfolio />
         </div>
     );
 }
---
+++ src/components/portfolio.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class portfolio extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Portfolio Page</h1>
+            </div>
+        )
+    }
+}
