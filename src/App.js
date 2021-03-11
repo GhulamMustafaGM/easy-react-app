178352d4cf70c2cdeb932a282c47dd6948974279
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-11 18:29:15 +0100

    created about component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './components/about';
 
 function App() {
     return ( 
         <div className="App">
             <h1>Hi World!</h1>
+            <About/>
         </div>
     );
 }
