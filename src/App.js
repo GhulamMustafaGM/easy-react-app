7a9563560f0f3b5b003c4d74662f5a59561c721c
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-11 20:03:22 +0100

    added UI

--- src/App.js
+++ src/App.js
@@ -3,40 +3,52 @@ import About from './components/about';
 import Contact from './components/contact';
 import Portfolio from './components/portfolio';
 import Home from './components/home';
-import { 
+import {
     BrowserRouter as Router,
     Route, Switch, Link
 } from 'react-router-dom';
 
 function App() {
-    return ( 
+    return (
         <Router>
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="/">Easy App</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
-                        <li>
-                            <Link to="/about">About</Link>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">About</a>
                         </li>
-                        <li>
-                            <Link to="/contact">Contact</Link>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/portfolio">Portfolio</a>
                         </li>
-                        <li>
-                            <Link to="/Portfolio">Portfolio</Link>
+                        <li className="nav-item">
+                        <a className="nav-link" href="/contact">Contact</a>
                         </li>
                     </ul>
-                </nav>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/about" component={About} />
                     <Route exact path="/contact" component={Contact} />
                     <Route exact path="/portfolio" component={Portfolio} />
-                </Switch>
-            </div>
+                </Switch>  
+            </div>    
         </Router>
     );
 }
-
 export default App;
