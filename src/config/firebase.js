9c446a3e5fd0aaa8b00ed255d342648737dcaec0
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-11 23:38:16 +0100

    created firebase realtime database

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBfHEEQ2Bu3hOxdYFjyRRoV7Uaepu-jBaQ",
+    authDomain: "easyapp-database.firebaseapp.com",
+    databaseURL: "https://easyapp-database-default-rtdb.firebaseio.com",
+    projectId: "easyapp-database",
+    storageBucket: "easyapp-database.appspot.com",
+    messagingSenderId: "748907258",
+    appId: "1:748907258:web:d185c89e65ec20ca00b743",
+    measurementId: "G-39QZMLMGQT"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
