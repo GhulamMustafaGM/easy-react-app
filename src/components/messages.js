277b4c124320802f6eb34a1687ce3553cbd2dadf
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-12 17:47:46 +0100

    created fetch data from firebase realtime database

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import myFirebase from '../config/firebase';
 
-const Messages = () => { 
+const Messages = () => {
+    useEffect(() => {
+        // alert('Message component is rendered!')
+        myFirebase.database().ref(`contacts`)
+        .on('value',(contacts) => {
+            contacts.forEach((contact) => {
+            console.log(`Each Contact -- ${contact.val().name}`)
+            })
+        })
+    }, [])
         return (
             <div>
                 <h1>Messages</h1>
             </div>
-        );
+        )
     
 }
 
