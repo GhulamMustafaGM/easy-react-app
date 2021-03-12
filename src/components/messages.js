7963c670ebde41f3d80ce8b7f4faa9353bd22014
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-12 18:09:00 +0100

    created display data on the screen

--- src/components/messages.js
+++ src/components/messages.js
@@ -2,21 +2,41 @@ import React, { useState, useEffect } from 'react';
 import myFirebase from '../config/firebase';
 
 const Messages = () => {
+    const [messages, setMessages] = useState([])
+
     useEffect(() => {
         // alert('Message component is rendered!')
         myFirebase.database().ref(`contacts`)
         .on('value',(contacts) => {
+            let msgs = []
             contacts.forEach((contact) => {
-            console.log(`Each Contact -- ${contact.val().name}`)
+                // console.log(`Each Contact -- ${contact.val().name}`)
+                msgs.push({
+                    name:contact.val().name,
+                    email:contact.val().email,
+                    message:contact.val().message
+                })
             })
+            setMessages(msgs)
         })
     }, [])
         return (
-            <div>
+            <div className="container">
                 <h1>Messages</h1>
+                {
+                    messages.map((message) =>  {
+                    return (
+                        <div>
+                        <p>{message.name}</p>
+                        <p>{message.email}</p>  
+                        <p>{message.message}</p>
+                        <hr />
+                    </div>
+                    )
+                    })
+                }
             </div>
         )
-    
 }
 
 export default Messages;
