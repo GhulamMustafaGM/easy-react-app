673b84eaa414f80ee97f74f1f66b2bda953ca879
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-13 22:22:52 +0100

    created routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+// routes
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/about', (req, res) => {
+    res.send('This is about route');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port!`);
 });
