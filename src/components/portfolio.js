7c1ed935b91f977f08e28bc6b9b78be0c5953ede
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-13 23:28:56 +0100

    created display data on the screen

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,12 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import user from '../../backend/models/user';
 
 const Portfolio = () => {
-    const [users,setUsers] = useState([])
+    const [users,setUsers] = useState({
+        id: '',
+        name: '',
+        date: '',
+        age: null
+    })
     useEffect(() => {
+        // making GET request
         axios.get('/users')
         .then((users) => {
-            console.log(users)
+            console.log(users.data)
+            setUsers({
+                id:users.data._id,
+                name:users.data.name,
+                date:users.data.date,
+                age:users.data.age
+            })
         })
         .catch((e) => console.log(e))
     }, [])
@@ -14,6 +27,8 @@ const Portfolio = () => {
         return (
             <div>
                 <h1>Portfolio Page</h1>
+                <p>{users.name}</p>
+                <p>{users.age}</p>
             </div>
         )
 }
